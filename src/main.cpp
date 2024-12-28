4c79de55beaf19970f3b069ee05abab03f37f344
Eduard Dlabal <eduard.dlabal@edcs.cz>
2024-12-28 10:30:17 +0100

    /

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,82 @@
-// change wifi credentials and mqtt settings to match your setup
-// some debug serial prints are commented out, you can uncomment them if you like to 
-
-#include "Connectivity.h"
+// change MQTT topics to your own
 
 #include <Arduino.h>
 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
 #include <ESP8266WiFi.h>
+#include "Connectivity.h"
 #include <PubSubClient.h>
 
-// defines pin for indication LED
+// defines delay between measurements in milliseconds
+// you can change this if you like to
+#define DELAY 1000
+
+// defines pin for indication LED and DS18B20 sensor
 #define LED_PIN 12
+const int oneWireBus = 14;
 
-WiFiClient espClient;
-PubSubClient mqtt_client(espClient);
+// creates object for oneWire bus
+OneWire oneWire(oneWireBus);
 
-// wifi credentials
-const char *ssid = "YOUR WIFI SSID";
-const char *password = "YOUR WIFI PASSWORD";
+// creates object for DS18B20 sensor
+DallasTemperature temp(&oneWire);
 
-// mqtt settings
-const char *mqtt_server = "YOUR BROKER";
-const int mqtt_port = 1883;
+// creates object for BMP280 sensor
+Adafruit_BMP280 bmp;
 
-// defines function to connect to WiFi
-void connectToWiFi()
+void setup()
 {
-    WiFi.begin(ssid, password);
-    Serial.print("connecting");
-    // waits until wifi is connected
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("\nWIFI: successfully connected");
+  // sets LED pin as output
+  pinMode(LED_PIN, OUTPUT);
+
+  // sets baudrate for serial communication
+  Serial.begin(115200);
+
+  // starts communication with DS18B20 sensor
+  temp.begin();
+
+  // starts communication with BMP280 sensor
+  // note: 0x76 is default I2C address for BMP280 sensor, but it may differ in other cases
+  if (!bmp.begin(0x76))
+    while (1)
+      ;
+
+  // calls function to connect to WiFi - function is declared in Connectivity.h
+  connectToWiFi();
+
+  // calls function to connect to MQTT broker
+  mqtt_client.setServer(mqtt_server, mqtt_port);
+
+  // prints message to serial monitor and blinks LED to indicate that setup is done
+  Serial.println("setup done");
+  digitalWrite(LED_PIN, HIGH);
+  delay(200);
+  digitalWrite(LED_PIN, LOW);
 }
 
-// defines function to connect to MQTT broker
-void connectToMQTTBroker()
+void loop()
 {
-    while (!mqtt_client.connected())
-    {
-        String client_id = "esp8266-" + String(WiFi.macAddress());
-        // Serial.printf("connecting to MQTT Broker as %s.....\n", client_id.c_str());
-        if (mqtt_client.connect(client_id.c_str()))
-        {
-            // Serial.println("MQTT: succesfully connected to broker");
-        }
-        else
-        {
-            Serial.print("Failed to connect to MQTT broker, rc=");
-            Serial.print(mqtt_client.state());
-            Serial.println(" try again in 5 seconds");
-            delay(5000);
-        }
-    }
-}
+  // tells sensors to measure
+  temp.requestTemperatures();
+  bmp.takeForcedMeasurement();
 
-// defines function to publish messages to MQTT topic
-void publishToMQTTBroker(const char *topic, const char *message)
-{
-    // checks if wifi is connected
-    if (WiFi.status() != WL_CONNECTED)
-        connectToWiFi();
+  // declares and defines variables for temperature and pressure measured by sensors
+  float DS18B20_temperature = temp.getTempCByIndex(0);
+  float BMP280_temperature = bmp.readTemperature();
+  float BMP280_pressure = bmp.readPressure() / 100.0F;
 
-    // checks if mqtt client is connected
-    if (!mqtt_client.connected())
-        connectToMQTTBroker();
+  // calls function to publish data to MQTT broker
+  // first argument is topic, you would probably like to change it
+  publishToMQTTBroker("meteo/bmptemperature", String(BMP280_temperature).c_str());
+  publishToMQTTBroker("meteo/pressure", String(BMP280_pressure).c_str());
+  publishToMQTTBroker("meteo/dallastemperature", String(DS18B20_temperature).c_str());
 
-    // publishes message to mqtt topic
-    mqtt_client.publish(topic, message);
-
-    // sends debug message to serial monitor
-    Serial.printf("MQTT: published to %s: %s\n", topic, message);
-
-    // blinks LED to indicate that message was published
-    digitalWrite(LED_PIN, HIGH);
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(100);
+  // waits for a second before repeating the loop
+  delay(DELAY);
 }
